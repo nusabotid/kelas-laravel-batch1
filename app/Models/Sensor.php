d9bced9d3f985d231ef99e6892e34b62023ca48f
FigoArbiansyah <figoarbnsyh@gmail.com>
2024-08-25 21:54:02 +0700

    Tentang Migration, Query Builder, Model, Factory, dan Eloquent

---
+++ app/Models/Sensor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+    use HasFactory;
+
+    protected $table = "sensors";
+
+    protected $fillable = [
+        'nama_sensor', 'data',
+    ];
+}
